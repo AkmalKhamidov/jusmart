d756997df0962b11e0b38c83cfcd192f3de10aa5
Akmal Khamidov <akmal_khamidov@epam.com>
2023-10-30 11:40:26 +0500

    Setting spring boot for tomcat server

--- src/main/java/com/example/justmart/JusMartApplication.java
+++ src/main/java/com/example/justmart/JusMartApplication.java
@@ -2,11 +2,19 @@ package com.example.justmart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JusMartApplication {
+public class JusMartApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder
+														 application) {
+		return application.sources(JusMartApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(JusMartApplication.class, args);
 	}
 
